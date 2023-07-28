332906775e1c8e0f2f74cd19e064b52995d2ef86
patrykwieteska <patrykwieteskapw@gmail.com>
2023-07-28 15:16:25 +0200

    Fisrt commit

---
+++ api/src/main/java/pl/spoda/ks/api/KsSpodaApplication.java
@@ -0,0 +1,13 @@
+package pl.spoda.ks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsSpodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsSpodaApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/pl/spoda/ks/api/KsSpodaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.spoda.ks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsSpodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
