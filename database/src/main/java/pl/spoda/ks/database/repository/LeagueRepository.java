4bd4f69983588209c0c1592ef54608924be25cd7
patrykwieteska <patrykwieteskapw@gmail.com>
2023-10-09 16:12:56 +0200

    modyfikacja beana v5

--- database/src/main/java/pl/spoda/ks/database/repository/LeagueRepository.java
+++ database/src/main/java/pl/spoda/ks/database/repository/LeagueRepository.java
@@ -2,12 +2,11 @@ package pl.spoda.ks.database.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+
 import pl.spoda.ks.database.entity.League;
 
 import java.util.Optional;
 
-@Component
 public interface LeagueRepository extends JpaRepository<League,Integer> {
 
     Optional<League> findByName(String name);
